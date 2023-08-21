d415356c5fc6fefb5224e66553e4f7df7b72c792
vshlkumar <75826289+vshlkumar@users.noreply.github.com>
2023-08-21 16:32:04 +0530

    create instance lifetime

---
+++ Dapper.Services/Interface/IInstanceLifetimeService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper.Services.Interface
+{
+    public interface IInstanceLifetimeService
+    {
+        string getTransient();
+
+        string getScoped();
+
+        string getSingleton();
+    }
+}
